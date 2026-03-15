f8adff017410217f98722cc92e4e373da308316f
MatveyZK <zharkov.matvey@internet.ru>
2026-03-15 15:19:13 +0000

    Formated with clang

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <string>
 
-int main(){
-	std::string name; //Comments
-	std::cout<<"Your name: "; //Comments
-	std::getline(cin, name); //Comments
-	std::cout<<"Hello world from "<<name<<std::endl; //Comments
+int
+main()
+{
+  std::string name;           // Comments
+  std::cout << "Your name: "; // Comments
+  std::getline(cin, name);    // Comments
+  std::cout << "Hello world from " << name << std::endl; // Comments
 }
