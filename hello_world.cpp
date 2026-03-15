205d4da0240847aefa93fcd93d9fcb04d9d8f8f9
MatveyZK <zharkov.matvey@internet.ru>
2026-03-15 14:20:16 +0000

    Added comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,8 +2,8 @@
 #include <string>
 
 int main(){
-	std::string name;
-	std::cout<<"Your name: ";
-	std::getline(cin, name); 
-	std::cout<<"Hello world from "<<name<<std::endl;
+	std::string name; //Комментарии
+	std::cout<<"Your name: "; //Комментарии
+	std::getline(cin, name); //Комментарии
+	std::cout<<"Hello world from "<<name<<std::endl; //Комментарии
 }
